0497fddd722db4b5af9a3e2683cd2c7bc74f7488
donjeta morina <donjeta.morina9@student.uni-pr.edu>
2026-06-13 23:09:29 -0400

    task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,10 +52,9 @@ public_users.get('/author/:author', function (req, res) {
 // Task 4: Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   const requestedTitle = req.params.title.toLowerCase();
-  const keys = Object.keys(books); // Obtain all keys from the books database
+  const keys = Object.keys(books); 
   const matchingBooks = {};
 
-  // Iterate and check if the book title matches the requested title
   keys.forEach(key => {
     if (books[key].title.toLowerCase() === requestedTitle) {
       matchingBooks[key] = books[key];
@@ -70,10 +69,18 @@ public_users.get('/title/:title', function (req, res) {
   }
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 5: Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn; // Retrieve the ISBN from request parameters
+  const book = books[isbn];    // Locate the book matching the ISBN
+
+  if (book) {
+    res.setHeader('Content-Type', 'application/json');
+    // Return only the reviews object belonging to that specific book
+    return res.status(200).send(JSON.stringify(book.reviews, null, 4));
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
