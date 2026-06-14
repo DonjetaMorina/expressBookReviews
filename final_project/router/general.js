fe04ac5f6c7bb9f9c7ed6c71bd42e994929e985f
donjeta morina <donjeta.morina9@student.uni-pr.edu>
2026-06-13 22:59:17 -0400

    task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,18 +10,24 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  // Set the header to JSON and send the cleanly stringified books object
+// Task 1: Get the book list available in the shop
+public_users.get('/', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+// Task 2: Get book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn; 
+  const book = books[isbn];    
+
+  if (book) {
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(200).send(JSON.stringify(book, null, 4)); 
+  } else {
+    return res.status(404).json({ message: "Book not found" }); 
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
